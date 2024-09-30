520350a391ee1d2aa313572f815db002143e1ca8
jinsigna15 <jinsigna15@cuc.edu.co>
2024-09-29 19:32:56 -0500

    pirmera version

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/favicon.ico'
+import viteLogo from '/Arkham.ico'
+import miImagen from './assets/mi-imagen.jpg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Bienvenido a la Milicia de Arkham</h1>
+      <img src={miImagen} alt= "Soldados de la milicia de arkham" className="custom-image" />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
